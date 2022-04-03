385a9bd5bff1f8885948fd38072d1ca9b034e26d
niiquash <qua16006@byui.edu>
2022-04-02 21:53:59 -0600

    Added dates to the top of the website

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,7 @@ function toggleMenu() {
 // Adding a button event listener to nav button
 const navBtn = document.getElementById("nav-btn");
 navBtn.onclick = toggleMenu;
+
+// Display Today's date to the user
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById("today-date").innerHTML = new Date().toLocaleDateString('en-US', options);
